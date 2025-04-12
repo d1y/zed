2f201cfdd7d4122004e47b9e5d52de1a2798e179
chz <chz@x.com>
2025-04-12 16:59:30 +0800

    Workaround windows lsp args paths

--- crates/language_extension/src/extension_lsp_adapter.rs
+++ crates/language_extension/src/extension_lsp_adapter.rs
@@ -156,7 +156,22 @@ impl LspAdapter for ExtensionLspAdapter {
 
             Ok(LanguageServerBinary {
                 path,
-                arguments: command.args.into_iter().map(|arg| arg.into()).collect(),
+                arguments: command
+                    .args
+                    .into_iter()
+                    .map(|arg| {
+                        #[cfg(windows)]
+                        {
+                            use regex::Regex;
+                            let pattern = r"^/[A-Z]:\\";
+                            let regex = Regex::new(pattern).unwrap();
+                            if regex.is_match(&arg) {
+                                return arg.trim_start_matches('/').into();
+                            }
+                        }
+                        return arg.into();
+                    })
+                    .collect(),
                 env: Some(command.env.into_iter().collect()),
             })
         }
